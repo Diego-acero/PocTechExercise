f62f79af0aebcc3dd1d9bdcffb53d51f006b79c5
diego <diegoaceroarguelles@gmail.com>
2020-07-23 11:44:33 +0200

    Transaction State class and missing fixtures for Testing

---
+++ src/main/java/com/acero/iobuilder/tech/exercise/domain/transactions/TransactionState.java
@@ -0,0 +1,14 @@
+package com.acero.iobuilder.tech.exercise.domain.transactions;
+
+public class TransactionState {
+    private String state;
+
+    public TransactionState(String state) {
+        this.state = state;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+}
